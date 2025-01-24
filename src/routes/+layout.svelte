513bfaf49f0a7ade9883d5651eccec6a563ec335
Banerjee Aditya <aditya.banerjee@activate.sg>
2025-01-24 12:01:02 +0800

    add placeholder layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main>
+	<div id="main-container" class="grid-rows-[10%,80%,10%] m-0 grid min-w-[100dvw] min-h-[100dvh]">
+		<div id="header">
+			<p>header placeholder</p>
+		</div>
+		{@render children()}
+		<div id="footer">
+			<p>footer placeholder</p>
+		</div>
+	</div>
+</main>
