9e408e9ebfea784a4e50bf0e9ab5b658e5fa90ff
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-25 20:49:20 +0800

    fix dark mode toggle

--- src/lib/context/theme.ts
+++ src/lib/context/theme.ts
@@ -1,11 +1,3 @@
 import { writable } from 'svelte/store';
 
 export const theme = writable<'light' | 'dark'>('dark');
-
-theme.subscribe((value) => {
-	if (value === 'light') {
-		document.documentElement.classList.remove('dark');
-	} else {
-		document.documentElement.classList.add('dark');
-	}
-});
