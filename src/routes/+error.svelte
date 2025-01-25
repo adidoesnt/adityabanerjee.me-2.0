702ad65ca2fd7130c6eeec5c3f198c5f905781d2
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-25 23:41:36 +0800

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import config from '$lib/config.json';
+
+    const { emojis } = config;
+</script>
+
+<main>
+	<div class="flex flex-col gap-2 h-full w-full items-center justify-center">
+		<h1 class="text-3xl">Error: {page.status}</h1>
+		<p class="text-2xl">{page.error?.message} {emojis.sad}</p>
+	</div>
+</main>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts"></script>
+
+<main>
+	<div class="flex h-full w-full items-center justify-center">
+		<h1>Placeholder</h1>
+	</div>
+</main>
