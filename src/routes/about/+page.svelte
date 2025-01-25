3ce47211aecf61268d31fb9ad98f429cbcf721a2
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-26 00:43:24 +0800

    add about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,11 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Input from "$lib/components/promptInput.svelte";
+	import PromptIcon from "$lib/assets/prompt-icon.svelte";
+</script>
 
 <main>
-	<div class="flex h-full w-full items-center justify-center">
-		<h1>Placeholder</h1>
+	<div class="flex flex-col h-full w-full items-center justify-center gap-24">
+		<PromptIcon />
+		<Input />
 	</div>
 </main>
