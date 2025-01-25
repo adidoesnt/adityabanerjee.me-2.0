def74a95d32c775057e11efc0836e732a4a3b6e6
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-25 20:56:36 +0800

    change dark mode icon colour depending on theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 	import Header from '$lib/components/header.svelte';
 	import '../app.css';
+	import { theme } from '$lib/context/theme';
+
+	const className = $derived(
+		$theme === 'dark'
+			? 'grid-rows-[10%,80%,10%] m-0 px-16 py-8 grid min-w-[100dvw] min-h-[100dvh] bg-background text-text font-itim'
+			: 'grid-rows-[10%,80%,10%] m-0 px-16 py-8 grid min-w-[100dvw] min-h-[100dvh] bg-text text-background font-itim'
+	);
+
 	let { children } = $props();
 </script>
 
 <main>
-	<div id="main-container" class="grid-rows-[10%,80%,10%] m-0 px-16 py-8 grid min-w-[100dvw] min-h-[100dvh] bg-background text-text font-itim">
+	<div
+		id="main-container"
+		class={className}
+	>
 		<Header />
 		{@render children()}
 		<div id="footer">
