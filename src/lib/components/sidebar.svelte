02ecc0e9acbbeabaef215bc2890eedb70dd28144
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-27 01:05:02 +0800

    add sidebar

---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import SidebarIcon from '$lib/assets/sidebar-icon.svelte';
+	import { theme } from '$lib/context/theme';
+
+	const sideBarClass = $derived(
+		$theme === 'dark'
+			? 'fixed left-0 flex flex-col items-center gap-8 w-fit max-w-[25dvw] p-8 h-full bg-primary text-text'
+			: 'fixed left-0 flex flex-col items-center gap-8 w-fit max-w-[25dvw] p-8 h-full bg-accent text-background'
+	);
+	let show = $state(false);
+	let { title, items } = $props();
+</script>
+
+<SidebarIcon
+	className="fixed w-8 h-8 left-0 top-0 z-10 m-4"
+	onclick={() => {
+		show = !show;
+	}}
+	{show}
+/>
+
+{#if show}
+	<div id="sidebar" class={sideBarClass}>
+		<h1 class="mt-16 text-4xl underline">{title}</h1>
+		<div id="posts" class="flex flex-col gap-4">
+			{#each items as item}
+				<div class="flex flex-col gap-2">
+					<h1 class="text-xl">{item.name}</h1>
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,9 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Sidebar from '$lib/components/sidebar.svelte';
+	import { blog } from '$lib/config.json';
+</script>
 
 <div class="flex h-full w-full items-center justify-center">
+	<Sidebar title="Posts" items={blog.posts} />
 	<h1 class="text-4xl">👷🏽‍♂️ Under Construction... 🚧</h1>
 </div>
