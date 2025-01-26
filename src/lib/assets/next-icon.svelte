083953154c97544158e71bc3ddb51ccfacc21c5f
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-26 19:07:18 +0800

    fix disabled styling

--- src/lib/assets/next-icon.svelte
+++ src/lib/assets/next-icon.svelte
@@ -4,18 +4,7 @@
 </script>
 
 <button aria-label="Send Prompt" {onclick} {disabled}>
-	{#if disabled}
-		<svg
-			fill="#EBEBEB"
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 50 50"
-			class={className}
-		>
-			<path
-				d="M9,46h32c2.761,0,5-2.239,5-5V9c0-2.761-2.239-5-5-5H9C6.239,4,4,6.239,4,9v32C4,43.761,6.239,46,9,46z M34,25l-14,9V16 L34,25z"
-			/></svg
-		>
-	{:else if $theme === 'dark'}
+	{#if $theme === 'dark'}
 		<svg fill="#f7e4d2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" class={className}>
 			<path
 				d="M9,46h32c2.761,0,5-2.239,5-5V9c0-2.761-2.239-5-5-5H9C6.239,4,4,6.239,4,9v32C4,43.761,6.239,46,9,46z M34,25l-14,9V16 L34,25z"
