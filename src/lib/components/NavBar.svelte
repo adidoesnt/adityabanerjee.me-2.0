88b7fb14da7229d6cf5bc086442ab8aae98d0ff2
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-05 12:57:51 +0800

    add home page link to hamburger menu

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -8,8 +8,19 @@
 	const { hamburgerMenu = false } = $props();
 </script>
 
-<div id="navbar" class={`${hamburgerMenu ? "flex flex-col" : "hidden"} lg:flex`}>
-	<ul class={`${hamburgerMenu ? "flex flex-col gap-4" : "flex gap-16"} items-center justify-between`}>
+<div id="navbar" class={`${hamburgerMenu ? 'flex flex-col' : 'hidden'} lg:flex`}>
+	<ul
+		class={`${hamburgerMenu ? 'flex flex-col gap-4' : 'flex gap-16'} items-center justify-between`}
+	>
+		{#if hamburgerMenu}
+			<li>
+				<a
+					href="/"
+					class={`bg-transparent text-xl font-bold ${pathname === '/' ? 'underline' : ''}`}>Home</a
+				>
+			</li>
+		{/if}
+
 		{#each navbar.items as item}
 			{#if item.href === pathname}
 				<li>
