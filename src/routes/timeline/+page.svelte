0b1a8370cc905a7a1a31ca9d495afd54714dd87b
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-05 18:59:51 +0800

    add mobile timeline

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -2,18 +2,18 @@
 	import { timeline } from '$lib/config.json';
 	import { theme } from '$lib/context/theme';
 	import DesktopTimeline from './components/DesktopTimeline.svelte';
+	import MobileTimeline from './components/MobileTimeline.svelte';
 
 	const cardClass = $derived(
-		$theme === 'dark' ? 'py-4 px-8 bg-beige text-darkBrown rounded-md' : 'py-4 px-8 bg-brown text-cream rounded-md'
+		$theme === 'dark'
+			? 'py-4 px-8 bg-beige text-darkBrown rounded-md'
+			: 'py-4 px-8 bg-brown text-cream rounded-md'
 	);
 
-	const timelineClass = $derived(
-		$theme === 'dark'
-			? 'bg-beige'
-			: 'bg-brown'
-	);
+	const timelineClass = $derived($theme === 'dark' ? 'bg-beige' : 'bg-brown');
 </script>
 
 <div class="flex h-full w-full items-start justify-center overflow-auto">
 	<DesktopTimeline {timeline} {cardClass} {timelineClass} />
+	<MobileTimeline {timeline} {cardClass} {timelineClass} />
 </div>
---
+++ src/routes/timeline/components/MobileTimeline.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	const { timeline, cardClass, timelineClass } = $props();
+</script>
+
+<div class="m-4 grid w-full grid-cols-[25%,75%] justify-center md:hidden">
+	{#each timeline as item}
+		<div class="flex w-full flex-col items-center justify-center">
+			<div class={`z-0 flex h-[45%] w-2 opacity-50 ${timelineClass}`}></div>
+			<div class={`flex aspect-square h-6 rounded-full ${timelineClass}`}></div>
+			<div class={`z-0 flex h-[45%] w-2 opacity-50 ${timelineClass}`}></div>
+		</div>
+		<div
+			class={`flex min-h-[20dvh] flex-col items-start justify-center gap-2 justify-self-start ${cardClass} my-4`}
+		>
+			<div class="flex h-full w-full flex-col items-center justify-center gap-y-2">
+                <div class="flex w-full justify-between items-center">
+                    <h1 class="text-center text-xl underline">{item.name}</h1>
+                    <p class="text-center text-sm">{item.date}</p>
+                </div>
+				<p>{item.description}</p>
+			</div>
+		</div>
+	{/each}
+</div>
