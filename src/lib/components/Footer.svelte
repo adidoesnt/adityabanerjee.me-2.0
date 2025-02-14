5d10202e688308bc3cf04fd0635b8050c4ea0e6e
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-14 19:07:56 +0000

    add icons credit

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,7 +6,7 @@
 	import FacebookLogo from '$lib/assets/facebook-logo.svelte';
 </script>
 
-<div id="footer" class="flex items-center md:px-16">
+<div id="footer" class="flex flex-col items-center gap-4 md:px-16">
 	<ul class="flex w-full items-center justify-between gap-4">
 		<li><GithubLogo /></li>
 		<li><LinkedinLogo /></li>
@@ -15,3 +15,8 @@
 		<li><InstagramLogo /></li>
 	</ul>
 </div>
+
+<p class="absolute bottom-0 right-0 m-2 mx-4 text-xs opacity-50 md:text-base">
+	Icons by <a class="underline hover:opacity-50" href="https://icons8.com" target="_blank">Icons8</a
+	>
+</p>
