289bb75409d62ee9613c6eb4d57cc741acdce5ba
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-27 18:04:04 +0000

    add placeholder blog page

--- src/lib/assets/sidebar-icon.svelte
+++ src/lib/assets/sidebar-icon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { theme } from '$lib/context/theme';
 	let { className = 'w-12 h-12', onclick, disabled = false, show = true } = $props();
-	className = `${className} opacity-50 hover:opacity-100 transition-opacity duration-300`;
+	className = `${className} opacity-50 hover:opacity-100 transition-opacity duration-300 z-50`;
 </script>
 
 <button aria-label="Toggle Sidebar" {onclick} {disabled}>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import config from '$lib/config.json';
+	import { theme } from '$lib/context/theme';
+
+	const cardBgAndTextClass = $derived(
+		$theme === 'dark' ? 'bg-brown text-cream' : 'bg-beige text-darkBrown'
+	);
+
+	const mockPosts = [
+		{
+			title: 'How to build a blog with SvelteKit',
+			date: 'Aug 2023'
+		},
+		{
+			title: 'How to build a blog with SvelteKit',
+			date: 'Aug 2023'
+		},
+		{
+			title: 'SvelteKit is the future of web development',
+			date: 'Jul 2023'
+		},
+		{
+			title: 'Why SvelteKit is the future of web development',
+			date: 'Jun 2023'
+		},
+		{
+			title: 'The future of web development',
+			date: 'May 2023'
+		},
+		{
+			title: 'How to build a blog with SvelteKit',
+			date: 'Aug 2023'
+		},
+		{
+			title: 'How to build a blog with SvelteKit',
+			date: 'Aug 2023'
+		},
+		{
+			title: 'SvelteKit is the future of web development',
+			date: 'Jul 2023'
+		},
+		{
+			title: 'Why SvelteKit is the future of web development',
+			date: 'Jun 2023'
+		},
+		{
+			title: 'The future of web development',
+			date: 'May 2023'
+		}
+	];
+</script>
+
+<div id="blog-page-container" class="flex flex-col w-full h-full justify-start items-center gap-8">
+	<h1 class="text-xl opacity-50">{config.blog.tooltip}</h1>
+	<div class="flex flex-wrap gap-4 overflow-auto justify-center">
+		{#each mockPosts as post}
+			<div class={`${cardBgAndTextClass} flex flex-col items-start justify-center gap-4 p-4 rounded-md w-60`}>
+				<h1 class="text-lg md:text-xl">{post.title}</h1>
+				<p class="text-sm">{post.date}</p>
+			</div>
+		{/each}
+	</div>
+</div>
