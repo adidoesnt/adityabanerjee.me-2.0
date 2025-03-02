5a05f8de30ef2fe1d33d1a32c6bc96d56d216f82
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-03-02 11:50:39 +0000

    remove unnecessary role policy attachment

--- infra/iam.tf
+++ infra/iam.tf
@@ -80,11 +80,6 @@ resource "aws_iam_policy" "abme_directus_ecs_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "abme_directus_ecs_task_policy_attachment" {
-  role       = aws_iam_role.abme_directus_ecs_task_role.name
-  policy_arn = aws_iam_policy.abme_directus_ecs_policy.arn
-}
-
 resource "aws_iam_role_policy_attachment" "abme_directus_ecs_execution_policy_attachment" {
   role       = aws_iam_role.abme_directus_ecs_execution_role.name
   policy_arn = aws_iam_policy.abme_directus_ecs_policy.arn
