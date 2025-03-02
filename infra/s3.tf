c315e52a333218a5659bfa0bd6f47762bda6d67e
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-03-02 11:21:57 +0000

    add s3 bucket for directus storage

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,3 +1,4 @@
+# Timeline images bucket
 resource "aws_s3_bucket" "timeline_images" {
   bucket = "abme-timeline-images"
 }
@@ -27,4 +28,41 @@ resource "aws_s3_bucket_policy" "public_read_policy" {
   })
 
   depends_on = [aws_s3_bucket_public_access_block.public_access_block]
+}
+
+
+# Directus bucket
+resource "aws_s3_bucket" "directus" {
+  bucket = "abme-directus"
+
+  tags = {
+    Name = "abme-directus"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "directus_public_access_block" {
+  bucket = aws_s3_bucket.directus.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "directus_public_read_policy" {
+  bucket = aws_s3_bucket.directus.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.directus.id}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.directus_public_access_block]
 }
